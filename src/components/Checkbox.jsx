74427c51d47ecc5a7248c21accf2142d1364dde1
Your Name <you@example.com>
2017-10-29 09:15:37 +0200

    first commit

---
+++ src/components/Checkbox.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class Checkbox extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            checked: this.props.inittiallyChecked
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(event){
+        this.setState({
+            checked: !this.state.checked
+        })
+    };
+
+    render(){
+        return(
+            <button className="checkbox icon" onClick={this.handleClick}>
+                <i className="material-icons">{this.state.checked ? 'check_box' : 'check_box_outline_blank'}</i>
+            </button>
+        );
+    }
+};
+
+Checkbox.propTypes = {
+    inittiallyChecked: React.PropTypes.bool.isRequired
+}
+
+export default Checkbox;
