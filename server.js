54a77b7ccf743812cd8d334bc71139bbea802991
Aleksey Dmytrenko <dieselokj@gmail.com>
2017-10-31 17:51:37 +0200

    Server in progress

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencode({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   res.setHeader('Cache-Control', 'no-cache');
